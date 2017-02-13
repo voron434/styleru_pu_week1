c4524fbaec09d1bb1558f725ce6c504bb35e2a98
voron434 <070809010@mail.ru>
2017-02-13 16:47:39 +0300

    fixing  3

--- Search_in_DataBase.py
+++ Search_in_DataBase.py
@@ -4,9 +4,6 @@ from helpers import search_for_film
 if __name__ == '__main__':
     Films_data = load_data()
     print('Enter film to search for:')
-    search = input().capitalize()
-    search1 = search.lower()
-    result = search_for_film(search1, Films_data) | search_for_film(search, Films_data)
-    for film in sorted(result):
+    search = input()
+    for film in sorted(search_for_film(search, Films_data)):
         print(film)
-

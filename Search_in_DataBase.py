4752b3c3fcf52f85ad903b0db035c78561783da1
voron434 <070809010@mail.ru>
2017-02-13 16:59:30 +0300

    finally fixed

--- Search_in_DataBase.py
+++ Search_in_DataBase.py
@@ -5,5 +5,8 @@ if __name__ == '__main__':
     Films_data = load_data()
     print('Enter film to search for:')
     search = input()
-    for film in sorted(search_for_film(search, Films_data)):
+    search1 = str(search[0]).capitalize() + search[1:len(search)]
+    search2 = str(search[0]).lower() + search[1:len(search)]
+    result = search_for_film(search1, Films_data) | search_for_film(search, Films_data)
+    for film in sorted(result):
         print(film)

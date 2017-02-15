8259202c2b0d94c6a2afa4c8b4c5b571a5e47c0e
voron434 <070809010@mail.ru>
2017-02-15 19:53:59 +0300

    Update search_in_DB.py

--- search_in_DB.py
+++ search_in_DB.py
@@ -2,11 +2,10 @@ from helpers import load_data
 from helpers import search_for_film
 
 if __name__ == '__main__':
-    Films_data = load_data()
+    films_data = load_data()
     print('Enter film to search for:')
     search = input()
-    big_first = str(search[0]).capitalize() + search[1:len(search)]
-    small_first = str(search[0]).lower() + search[1:len(search)]
-    result = search_for_film(big_first, Films_data) or search_for_film(small_first, Films_data)
+    result = search_for_film(search, films_data)
     for film in sorted(result):
         print(film)
+

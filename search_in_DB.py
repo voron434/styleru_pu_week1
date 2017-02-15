24d0d12431306900b33f2aaefa1e6a3d93633212
voron434 <070809010@mail.ru>
2017-02-15 12:47:55 +0300

    Update and rename Search_in_DataBase.py to search_in_DB.py

--- search_in_DB.py
+++ search_in_DB.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     Films_data = load_data()
     print('Enter film to search for:')
     search = input()
-    search1 = str(search[0]).capitalize() + search[1:len(search)]
-    search2 = str(search[0]).lower() + search[1:len(search)]
-    result = search_for_film(search1, Films_data) | search_for_film(search, Films_data)
+    big_first = str(search[0]).capitalize() + search[1:len(search)]
+    small_first = str(search[0]).lower() + search[1:len(search)]
+    result = search_for_film(big_first, Films_data) or search_for_film(small_first, Films_data)
     for film in sorted(result):
         print(film)

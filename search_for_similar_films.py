8c08843c334166823b479bbbf6ac7ba52dd49b09
voron434 <070809010@mail.ru>
2017-02-15 22:45:33 +0300

    rewrited recommend

--- search_for_similar_films.py
+++ search_for_similar_films.py
@@ -1,25 +1,15 @@
 from helpers import load_data
 from helpers import is_my_film_there
-from helpers import search_for_film
-from helpers import recommend_me_by
+from helpers import recommend
 
 if __name__ == '__main__':
     films_data = load_data()
     print('Enter film to search for:')
     search = input()
     my_film = is_my_film_there(search, films_data)
-    recommendation = search_for_film(search, films_data)
-    recommendation.remove(search)
-    num_of_films_to_recommend = 8
-
-    if len(recommendation) < num_of_films_to_recommend:
-        recommendation_by_budget = recommend_me_by(my_film, films_data, 'budget')
-        for film in range(len(recommendation_by_budget) - num_of_films_to_recommend + len(recommendation)):
-            recommendation_by_budget.pop()
+    recommendation = recommend(my_film, films_data)
 
     for film in sorted(recommendation):
-        print(film)
-    for film in sorted(recommendation_by_budget):
         print(film)
 
 

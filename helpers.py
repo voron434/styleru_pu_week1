48362883f64ab90286af2636bf4d58292a028cd6
voron434 <070809010@mail.ru>
2017-02-15 21:10:21 +0300

    redo some functions

--- helpers.py
+++ helpers.py
@@ -21,9 +21,9 @@ def make_tmdb_api_request(method, api_key, extra_params=None):
 
 def get_user_api_key():
     print('Enter your api key v3')
-    user_api_key = getpass('Enter your api key v3')
+    user_api_key = input() #use getpass()
     try:
-        make_tmdb_api_request(method='/movie/2', api_key=user_api_key)
+        make_tmdb_api_request(method='/movie/2', api_key = user_api_key)
     except urllib.error.HTTPError as err:
         if err.code == 401:
             print('Invalid api key')
@@ -37,28 +37,42 @@ def get_movie_info(movie_number, user_api_key):
 
 def get_movie_budget(movie_number, user_api_key):
     print(make_tmdb_api_request(method='/movie/%d' % movie_number, api_key=user_api_key)['budget'])
-    
+
+def save_films(user_api_key, num_of_films):
+    big_data = []
+    for film_id in range(num_of_films):
+        try:
+            big_data.append(get_movie_info(film_id, user_api_key))
+            print('id: %s saved' % str(film_id))
+        except urllib.error.HTTPError as err:
+            if err.code == 404:  #if no film on this id
+                print('id: %s is empty' % str(film_id))
+                continue
+            else:
+                print('smth wrong with code...')
+    return big_data
+
 #=======================================================================================================
 def load_data():
     print('Enter path to DataBase:')
     path = input()
     try:
-        my_file = open(path, mode='r', encoding='utf-8')
-        Films_data = json.load(my_file)
-        return Films_data
+        with open(path, mode='r', encoding='utf-8') as my_file:
+            films_data = json.load(my_file)
+            return films_data
     except FileNotFoundError:
         print('File not found, sorry...')
         raise SystemExit
-        
-def search_for_film(search, Films_data):
+
+def search_for_film(search, films_data):
     films_founded = set()
-    for film in Films_data:
-        if search in film['original_title']:
+    for film in films_data:
+        if search.lower() in film['original_title'].lower():
             films_founded.add(film['original_title'])
     return films_founded
 
-def is_my_film_there(search, Films_data):
-    for film in Films_data:
+def is_my_film_there(search, films_data):
+    for film in films_data:
         if search == film['original_title']:
             return film
     print('No such film in FilmsDB')

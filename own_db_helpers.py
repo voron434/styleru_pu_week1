0d83df2527a1fd23497c5741fd457ef347905067
voron434 <070809010@mail.ru>
2017-03-10 01:29:55 +0300

    forgot some detail

--- own_db_helpers.py
+++ own_db_helpers.py
@@ -5,5 +5,5 @@ def load_data(path):
     with open(path, mode='r', encoding='utf-8') as my_file:
             films_data = json.load(my_file)
             return films_data
-    if not os.path.exists():
+    if not os.path.exists(path):
         return None

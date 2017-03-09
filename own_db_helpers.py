9bc7347eeffa8dad99f57987fa7013e26e2b339b
voron434 <070809010@mail.ru>
2017-03-10 01:50:21 +0300

    just wrong queue of actions

--- own_db_helpers.py
+++ own_db_helpers.py
@@ -2,8 +2,8 @@ import json
 import os
 
 def load_data(path):
-    with open(path, mode='r', encoding='utf-8') as my_file:
-            films_data = json.load(my_file)
-            return films_data
     if not os.path.exists(path):
         return None
+    with open(path, mode='r', encoding='utf-8') as my_file:
+        films_data = json.load(my_file)
+        return films_data

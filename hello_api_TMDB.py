246b022540e0635cea7a559f7f5a82534f154a7d
voron434 <070809010@mail.ru>
2017-03-09 23:28:16 +0300

    making functions cleaner

--- hello_api_TMDB.py
+++ hello_api_TMDB.py
@@ -1,7 +1,11 @@
-from helpers import get_user_api_key
-from helpers import get_movie_budget
+from tmdb_helpers import get_user_api_key
+from tmdb_helpers import make_tmdb_api_request
 
 if __name__ == '__main__':
     user_api_key = get_user_api_key()
-    get_movie_budget(215, user_api_key)
+    if not user_api_key:
+        print('Invalid api key')
+        raise SystemExit
+    movie_number = 215
+    print(make_tmdb_api_request(method='/movie/%d' % movie_number, api_key=user_api_key)['budget'])
 

4d3a61671bee45f294280587fc71ea3f5e718142
voron434 <070809010@mail.ru>
2017-03-09 23:26:43 +0300

    adding splited functions from helpers

---
+++ own_db_helpers.py
@@ -0,0 +1,9 @@
+import json
+import os
+
+def load_data(path):
+    with open(path, mode='r', encoding='utf-8') as my_file:
+            films_data = json.load(my_file)
+            return films_data
+    if not os.path.exists():
+        return None
